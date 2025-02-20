a4c9e7c74ed5e9d2ab34b902ab74b37521671c75
Trixie Dolera <trixie.dolera@cit.edu>
2025-02-20 22:40:11 +0800

    Update Designer

--- Login.Designer.cs
+++ Login.Designer.cs
@@ -34,7 +34,8 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            BackColor = Color.MistyRose;
+            ClientSize = new Size(514, 558);
             Name = "Login";
             ShowIcon = false;
             ResumeLayout(false);
