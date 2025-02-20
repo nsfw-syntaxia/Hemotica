b92845007bbaf00c93a691f7e3e582849255b6ce
Trixie Dolera <trixie.dolera@cit.edu>
2025-02-20 22:49:17 +0800

    Add NuGet Package

--- Login.Designer.cs
+++ Login.Designer.cs
@@ -35,7 +35,7 @@
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.MistyRose;
-            ClientSize = new Size(514, 558);
+            ClientSize = new Size(488, 558);
             Name = "Login";
             ShowIcon = false;
             ResumeLayout(false);
