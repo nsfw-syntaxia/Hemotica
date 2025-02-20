1aa30287f52d75bfe0d0ffdf0032282853be3655
Trixie Dolera <trixie.dolera@cit.edu>
2025-02-20 22:56:16 +0800

    Update Designer

--- Login.Designer.cs
+++ Login.Designer.cs
@@ -28,7 +28,40 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Heebo", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(229, 120);
+            label1.Name = "label1";
+            label1.Size = new Size(67, 28);
+            label1.TabIndex = 1;
+            label1.Text = "LOGIN";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Heebo", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(88, 223);
+            label2.Name = "label2";
+            label2.Size = new Size(112, 28);
+            label2.TabIndex = 2;
+            label2.Text = "USERNAME";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Heebo", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(88, 286);
+            label3.Name = "label3";
+            label3.Size = new Size(112, 28);
+            label3.TabIndex = 3;
+            label3.Text = "PASSWORD";
             // 
             // Login
             // 
@@ -36,11 +69,19 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.MistyRose;
             ClientSize = new Size(488, 558);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
             Name = "Login";
             ShowIcon = false;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
     }
 }
