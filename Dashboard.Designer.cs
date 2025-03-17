7a908e07e191804c130448dca7ff924be6de0b37
Trixie Dolera <trixie.dolera@cit.edu>
2025-03-17 21:29:04 +0800

    Update Designer

---
+++ Dashboard.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Hemotica
+{
+    partial class Dashboard
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Dashboard));
+            btnClose = new Button();
+            btnMaximize = new Button();
+            btnMinimize = new Button();
+            SuspendLayout();
+            // 
+            // btnClose
+            // 
+            btnClose.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            btnClose.FlatStyle = FlatStyle.Flat;
+            btnClose.Image = (Image)resources.GetObject("btnClose.Image");
+            btnClose.Location = new Point(1226, 12);
+            btnClose.Name = "btnClose";
+            btnClose.Size = new Size(25, 25);
+            btnClose.TabIndex = 2;
+            btnClose.TabStop = false;
+            btnClose.UseVisualStyleBackColor = true;
+            btnClose.Click += btnClose_Click;
+            // 
+            // btnMaximize
+            // 
+            btnMaximize.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            btnMaximize.FlatStyle = FlatStyle.Flat;
+            btnMaximize.Image = (Image)resources.GetObject("btnMaximize.Image");
+            btnMaximize.Location = new Point(1195, 12);
+            btnMaximize.Name = "btnMaximize";
+            btnMaximize.Size = new Size(25, 25);
+            btnMaximize.TabIndex = 3;
+            btnMaximize.TabStop = false;
+            btnMaximize.UseVisualStyleBackColor = true;
+            btnMaximize.Click += btnMaximize_Click;
+            // 
+            // btnMinimize
+            // 
+            btnMinimize.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            btnMinimize.FlatStyle = FlatStyle.Flat;
+            btnMinimize.Image = (Image)resources.GetObject("btnMinimize.Image");
+            btnMinimize.ImageAlign = ContentAlignment.TopCenter;
+            btnMinimize.Location = new Point(1164, 12);
+            btnMinimize.Name = "btnMinimize";
+            btnMinimize.Size = new Size(25, 25);
+            btnMinimize.TabIndex = 4;
+            btnMinimize.TabStop = false;
+            btnMinimize.UseVisualStyleBackColor = true;
+            btnMinimize.Click += btnMinimize_Click;
+            // 
+            // Dashboard
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(253, 211, 211);
+            ClientSize = new Size(1263, 803);
+            ControlBox = false;
+            Controls.Add(btnMinimize);
+            Controls.Add(btnMaximize);
+            Controls.Add(btnClose);
+            DoubleBuffered = true;
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Dashboard";
+            StartPosition = FormStartPosition.CenterScreen;
+            Load += Home_Load;
+            MouseDown += Home_MouseDown;
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private Button btnClose;
+        private Button btnMaximize;
+        private Button btnMinimize;
+    }
+}
---
+++ Dashboard.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+
+namespace Hemotica
+{
+    public partial class Dashboard : Form
+    {
+        public Dashboard()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("user32.dll")]
+        private static extern int SendMessage(IntPtr hWnd, int msg, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll")]
+        private static extern bool ReleaseCapture();
+
+        private const int WM_NCLBUTTONDOWN = 0xA1;
+        private const int HTCAPTION = 0x2;
+
+        private void Home_Load(object sender, EventArgs e)
+        {
+            btnSettings();
+        }
+
+        private void Home_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(this.Handle, WM_NCLBUTTONDOWN, (IntPtr)HTCAPTION, IntPtr.Zero);
+            }
+        }
+
+        private void btnEffects(Button button, Color highlightColor)
+        {
+            button.FlatAppearance.BorderSize = 0;
+
+            button.MouseEnter += (s, e) => button.BackColor = Color.FromArgb(50, highlightColor);
+            button.MouseLeave += (s, e) => button.BackColor = Color.Transparent;
+        }
+
+        private void btnSettings()
+        {
+            btnEffects(btnClose, Color.Red);
+            btnEffects(btnMaximize, Color.Lime);
+            btnEffects(btnMinimize, Color.Yellow);
+        }
+
+        private void btnMinimize_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void btnMaximize_Click(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Maximized)
+                this.WindowState = FormWindowState.Normal;
+            else
+                this.WindowState = FormWindowState.Maximized;
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
