e8f9401bc4b9a04624c436b9cb6ffb74a98cb72e
Trixie Dolera <trixie.dolera@cit.edu>
2025-04-08 17:20:36 +0800

    Appointment Test Cases and UI Changes

--- DonorAppointments.cs
+++ DonorAppointments.cs
@@ -109,7 +109,7 @@ namespace Hemotica
 					panel.Controls.Add(lblStatus);
 
 					lblHospital.Location = new Point(20, lblDate.Bottom + 10);
-					lblStatus.Location = new Point(300, lblHospital.Top);
+					lblStatus.Location = new Point(350, lblHospital.Top);
 
 					if (status != "Denied" && status != "Cancelled")
 					{
